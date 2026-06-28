65321ea2720614a5c5bce0d2e75f0ab12eb446ed
Robert Penrod <robertpenrod42@gmail.com>
2026-06-28 19:02:36 -0400

    Theming

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
     Button,
     Container,
     Flex,
-    MantineProvider,
+    useMantineTheme,
 } from "@mantine/core";
 import { useState } from "react";
 import { GiMagicGate } from "react-icons/gi";
@@ -15,114 +15,118 @@ import "./App.css";
 function App() {
     const [opened, setOpened] = useState(false);
     const toggle = () => setOpened(!opened);
+    const useTheme = useMantineTheme();
 
     return (
-        <MantineProvider>
-            <AppShell
-                padding='md'
-                header={{ height: 60 }}
-                navbar={{
-                    width: 300,
-                    breakpoint: "sm",
-                    collapsed: { desktop: !opened, mobile: !opened },
+        <AppShell
+            padding='md'
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: "sm",
+                collapsed: { desktop: !opened, mobile: !opened },
+            }}
+            transitionDuration={375}
+            transitionTimingFunction='ease'
+        >
+            <AppShell.Header
+                c='white'
+                fw='bold'
+                style={{
+                    backgroundColor: useTheme.colors.main[9],
+                    fontSize: "19px",
                 }}
-                transitionDuration={375}
-                transitionTimingFunction='ease'
             >
-                <AppShell.Header>
-                    <Flex
-                        align='center'
-                        justify='space-between'
-                        h='100%'
-                        px='md'
-                    >
-                        <Burger
-                            opened={opened}
-                            onClick={toggle}
-                            // hiddenFrom='sm'
-                            size='sm'
-                        />
+                <Flex align='center' justify='space-between' h='100%' px='md'>
+                    <Burger
+                        opened={opened}
+                        onClick={toggle}
+                        // hiddenFrom='sm'
+                        size='sm'
+                        color='white'
+                    />
 
-                        <Flex gap='xs' w='100%' align='center' justify='center'>
-                            <GiMagicGate size={25} />
-                            <div>Artificial Adventure</div>
-                            <RiRobot2Fill size={25} />
-                        </Flex>
-
-                        <Avatar />
+                    <Flex gap='xs' w='100%' align='center' justify='center'>
+                        <GiMagicGate size={20} />
+                        <div>Artificial Adventure</div>
+                        <RiRobot2Fill size={20} />
                     </Flex>
-                </AppShell.Header>
 
-                <AppShell.Navbar>Navbar</AppShell.Navbar>
+                    <Avatar />
+                </Flex>
+            </AppShell.Header>
 
-                <AppShell.Main>
-                    <Container>
-                        <Flex
-                            justify='center'
-                            mt='md'
-                            direction='column'
-                            align='center'
-                            gap='xl'
-                        >
-                            <div>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipiscing elit. Quisque faucibus ex sapien
-                                vitae pellentesque sem placerat. In id cursus mi
-                                pretium tellus duis convallis. Tempus leo eu
-                                aenean sed diam urna tempor. Pulvinar vivamus
-                                fringilla lacus nec metus bibendum egestas.
-                                Iaculis massa nisl malesuada lacinia integer
-                                nunc posuere. Ut hendrerit semper vel class
-                                aptent taciti sociosqu. Ad litora torquent per
-                                conubia nostra inceptos himenaeos. Lorem ipsum
-                                dolor sit amet consectetur adipiscing elit.
-                                Quisque faucibus ex sapien vitae pellentesque
-                                sem placerat. In id cursus mi pretium tellus
-                                duis convallis. Tempus leo eu aenean sed diam
-                                urna tempor. Pulvinar vivamus fringilla lacus
-                                nec metus bibendum egestas. Iaculis massa nisl
-                                malesuada lacinia integer nunc posuere. Ut
-                                hendrerit semper vel class aptent taciti
-                                sociosqu. Ad litora torquent per conubia nostra
-                                inceptos himenaeos. Lorem ipsum dolor sit amet
-                                consectetur adipiscing elit. Quisque faucibus ex
-                                sapien vitae pellentesque sem placerat. In id
-                                cursus mi pretium tellus duis convallis. Tempus
-                                leo eu aenean sed diam urna tempor. Pulvinar
-                                vivamus fringilla lacus nec metus bibendum
-                                egestas. Iaculis massa nisl malesuada lacinia
-                                integer nunc posuere. Ut hendrerit semper vel
-                                class aptent taciti sociosqu. Ad litora torquent
-                                per conubia nostra inceptos himenaeos. Lorem
-                                ipsum dolor sit amet consectetur adipiscing
-                                elit. Quisque faucibus ex sapien vitae
-                                pellentesque sem placerat. In id cursus mi
-                                pretium tellus duis convallis. Tempus leo eu
-                                aenean sed diam urna tempor. Pulvinar vivamus
-                                fringilla lacus nec metus bibendum egestas.
-                                Iaculis massa nisl malesuada lacinia integer
-                                nunc posuere. Ut hendrerit semper vel class
-                                aptent taciti sociosqu. Ad litora torquent per
-                                conubia nostra inceptos himenaeos. Lorem ipsum
-                                dolor sit amet consectetur adipiscing elit.
-                                Quisque faucibus ex sapien vitae pellentesque
-                                sem placerat. In id cursus mi pretium tellus
-                                duis convallis. Tempus leo eu aenean sed diam
-                                urna tempor. Pulvinar vivamus fringilla lacus
-                                nec metus bibendum egestas. Iaculis massa nisl
-                                malesuada lacinia integer nunc posuere. Ut
-                                hendrerit semper vel class aptent taciti
-                                sociosqu. Ad litora torquent per conubia nostra
-                                inceptos himenaeos.
-                            </div>
+            <AppShell.Navbar
+                style={{ backgroundColor: useTheme.colors.main[8] }}
+                c='white'
+                p='sm'
+            >
+                Navbar
+            </AppShell.Navbar>
 
-                            <div>Choices</div>
-                            <Button>Submit</Button>
-                        </Flex>
-                    </Container>
-                </AppShell.Main>
-            </AppShell>
-        </MantineProvider>
+            <AppShell.Main style={{ backgroundColor: useTheme.colors.main[0] }}>
+                <Container>
+                    <Flex
+                        justify='center'
+                        mt='md'
+                        direction='column'
+                        align='center'
+                        gap='xl'
+                    >
+                        <div>
+                            Lorem ipsum dolor sit amet consectetur adipiscing
+                            elit. Quisque faucibus ex sapien vitae pellentesque
+                            sem placerat. In id cursus mi pretium tellus duis
+                            convallis. Tempus leo eu aenean sed diam urna
+                            tempor. Pulvinar vivamus fringilla lacus nec metus
+                            bibendum egestas. Iaculis massa nisl malesuada
+                            lacinia integer nunc posuere. Ut hendrerit semper
+                            vel class aptent taciti sociosqu. Ad litora torquent
+                            per conubia nostra inceptos himenaeos. Lorem ipsum
+                            dolor sit amet consectetur adipiscing elit. Quisque
+                            faucibus ex sapien vitae pellentesque sem placerat.
+                            In id cursus mi pretium tellus duis convallis.
+                            Tempus leo eu aenean sed diam urna tempor. Pulvinar
+                            vivamus fringilla lacus nec metus bibendum egestas.
+                            Iaculis massa nisl malesuada lacinia integer nunc
+                            posuere. Ut hendrerit semper vel class aptent taciti
+                            sociosqu. Ad litora torquent per conubia nostra
+                            inceptos himenaeos. Lorem ipsum dolor sit amet
+                            consectetur adipiscing elit. Quisque faucibus ex
+                            sapien vitae pellentesque sem placerat. In id cursus
+                            mi pretium tellus duis convallis. Tempus leo eu
+                            aenean sed diam urna tempor. Pulvinar vivamus
+                            fringilla lacus nec metus bibendum egestas. Iaculis
+                            massa nisl malesuada lacinia integer nunc posuere.
+                            Ut hendrerit semper vel class aptent taciti
+                            sociosqu. Ad litora torquent per conubia nostra
+                            inceptos himenaeos. Lorem ipsum dolor sit amet
+                            consectetur adipiscing elit. Quisque faucibus ex
+                            sapien vitae pellentesque sem placerat. In id cursus
+                            mi pretium tellus duis convallis. Tempus leo eu
+                            aenean sed diam urna tempor. Pulvinar vivamus
+                            fringilla lacus nec metus bibendum egestas. Iaculis
+                            massa nisl malesuada lacinia integer nunc posuere.
+                            Ut hendrerit semper vel class aptent taciti
+                            sociosqu. Ad litora torquent per conubia nostra
+                            inceptos himenaeos. Lorem ipsum dolor sit amet
+                            consectetur adipiscing elit. Quisque faucibus ex
+                            sapien vitae pellentesque sem placerat. In id cursus
+                            mi pretium tellus duis convallis. Tempus leo eu
+                            aenean sed diam urna tempor. Pulvinar vivamus
+                            fringilla lacus nec metus bibendum egestas. Iaculis
+                            massa nisl malesuada lacinia integer nunc posuere.
+                            Ut hendrerit semper vel class aptent taciti
+                            sociosqu. Ad litora torquent per conubia nostra
+                            inceptos himenaeos.
+                        </div>
+
+                        <div>Choices</div>
+                        <Button>Submit</Button>
+                    </Flex>
+                </Container>
+            </AppShell.Main>
+        </AppShell>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,31 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { createTheme, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+
+const theme = createTheme({
+    primaryColor: "main",
+    colors: {
+        main: [
+            "#d8ebee", // Main BG
+            "#5FCCDB",
+            "#44CADC",
+            "#2AC9DE",
+            "#1AC2D9",
+            "#11B7CD",
+            "#629aa1", // Button
+            "#0E99AC",
+            "#3c4e52", // Navbar
+            "#041a1d", // Header
+        ],
+    },
+});
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <MantineProvider theme={theme}>
+            <App />
+        </MantineProvider>
     </StrictMode>,
 );
